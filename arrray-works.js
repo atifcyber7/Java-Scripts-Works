dd1113791196fb5a847eb9a32d56aa583c570341
atifkhan2 <matifjan0987@gmail.com>
2023-11-19 12:38:02 +0500

    Array Works

---
+++ arrray-works.js
@@ -0,0 +1,18 @@
+// *************** Array works  *****************
+
+let students = ['Atif Zeb','Arshad ','Ali '," Umar"]
+// indecing/index      0         1         2       3
+alert(students[0])
+alert(students[1])
+
+
+// String Properties
+console.log(students[0].length)
+console.log(students[1].toLowerCase())
+
+
+
+// Array Propreties
+console.log(students.length)
+console.log(students.concat(['usama','khan']))
+students.push("Jawad")
