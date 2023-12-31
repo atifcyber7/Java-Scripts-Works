08fc3291d031b4cc4a60fdc4cd8888fb2c4f06ea
atifkhan2 <matifjan0987@gmail.com>
2023-12-31 12:25:55 +0500

    Update

---
+++ Prototype Work/Prototype.js
@@ -0,0 +1,42 @@
+function Person(name,age,gender,cnic){
+    this.name =name,
+    this.age = age,
+    this.gender=gender,
+    this.cnic=cnic
+}
+
+
+let p1 = new Person('kaleemullah',23,'male','12345')
+let p2 = new Person('Nasir',30,'male','456235')
+
+
+function Plan(name, price, space, transfer, pages, discountMonths) 
+{
+    this.name = name;
+    this.price = price;
+    this.space = space;
+    this.transfer = transfer;
+    this.pages = pages;
+    this.discountMonths = discountMonths;
+
+    }
+
+Plan.prototype.calcAnnual = function() 
+{
+var bestPrice = this.price;
+var currDate = new Date();
+var thisMo = currDate.getMonth()+1;
+
+    for (var i = 0; i < this.discountMonths.length; i++) 
+    {
+        if (this.discountMonths[i] === thisMo) 
+            {
+            bestPrice = this.price * .80;
+            break;
+            }
+    }
+    return bestPrice * 12;
+};
+
+myPlan = new Plan('Advance',99.99,1000,5000,500,[6,12])
+apkaPlan = new Plan('Basic',50.99,100,500,50,[1,6,12])
